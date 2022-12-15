909492efcf25c83d183a342a729f9d49a1597afe
MarketaH <habranova.marketa@gmail.com>
2022-12-15 20:22:36 +0100

    Added company list and company form.

--- jobsee/urls.py
+++ jobsee/urls.py
@@ -4,4 +4,6 @@ import jobsee.views as views
 
 urlpatterns = [
     path('', views.HomepageView.as_view(), name='homepage'),
+    path('companies', views.CompanyListView.as_view(), name='company_list'),
+    path('companies/create', views.CompanyCreateView.as_view(), name='company_create')
 ]
--- jobsee/views.py
+++ jobsee/views.py
@@ -1,5 +1,19 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, CreateView
+from django.urls import reverse_lazy
+import jobsee.models as models
 
 
 class HomepageView(TemplateView):
     template_name = 'base.html'
+
+
+class CompanyListView(ListView):
+    model = models.Company
+    template_name = 'company/list.html'
+
+
+class CompanyCreateView(CreateView):
+    model = models.Company
+    template_name = 'company/create.html'
+    fields = ['name', 'base_location', 'technologies', 'www']
+    success_url = reverse_lazy('company_list')
